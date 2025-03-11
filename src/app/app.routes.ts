5a940350a972f83c011beea7a3a9b0b144713018
Attentat1987 <129548674+Attentat1987@users.noreply.github.com>
2025-03-11 13:30:19 +0100

    ✨ Privacy Policy Seite und Routing hinzugefügt + Footer Link erstellt

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,12 +8,12 @@ import { ContactComponent } from './components/contact/contact.component';
 import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent }, // Startseite
-  { path: 'about', component: AboutComponent }, // Über mich
-  { path: 'projects', component: ProjectsComponent }, // Projekte
-  { path: 'contact', component: ContactComponent }, // Kontakt
+  { path: '', component: HomeComponent },                 // Startseite
+  { path: 'about', component: AboutComponent },           // Über mich
+  { path: 'projects', component: ProjectsComponent },     // Projekte
+  { path: 'contact', component: ContactComponent },       // Kontakt
   { path: 'privacy-policy', component: PrivacyPolicyComponent }, // Datenschutz
 
-  // Wildcard-Route (optional): Fängt alles ab, was es nicht gibt
+  // Wildcard-Route: Fängt alles ab, was nicht definiert ist und leitet zur Startseite
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
